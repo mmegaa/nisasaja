e510c253f95b9a82be77bdd7a84715c021d3e352
annisa mega <nisaebew56@gmail.com>
2026-05-18 12:06:59 +0700

    Add files via upload

---
+++ buku.php
@@ -0,0 +1,35 @@
+<?php
+include "../koneksi.php";
+$query = "SELECT * FROM tbl_buku";
+$data = mysqli_query($conn, $query);
+?>
+<h3>Daftar Buku Koleksi</h3>
+<h5><a href="index.php?page=bukutambah">[+] Tambah Buku</a></h5>
+
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>Judul</th>
+        <th>Pengarang</th>
+        <th>Penerbit</th>
+        <th>Aksi</th>
+    </tr>
+    <?php 
+    $nomor = 1;
+    while($row = mysqli_fetch_array($data)) {
+        echo "<tr>";
+        echo "<td> $nomor </td>";
+        echo "<td>". $row['judul'] ."</td>";
+        echo "<td>". $row['pengarang'] ."</td>";
+        echo "<td>". $row['penerbit'] ."</td>";
+        echo "<td>";
+        echo "<a href='../admin/index.php?page=bukuubah&idbuku=" .$row['idbuku']."'>Edit</a> |";
+        echo "<a href='halaman/buku/bukuhapus.php?idbuku=" .$row['idbuku']."'> Delete</a>";
+        echo "</td>";
+        echo "</tr>";
+        $nomor++;
+        
+    }
+    
+    ?>
+</table>
